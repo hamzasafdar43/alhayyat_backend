b73a95cdf65993b67bd9a5e77d20e2ac4e8ebf48
hamzasafdar43 <alihamza4443243@gmail.com>
2025-10-14 07:22:51 -0700

    filter carwash bill

--- src/routes/carWash/carwashRoutes.js
+++ src/routes/carWash/carwashRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { generatecarwashbillCtrl ,getallbills  ,  updateCarWashbill, deleteCarWashbill} = require("../../controller/carWash/carwashCtrl");
+const { generatecarwashbillCtrl ,getallbills  ,  updateCarWashbill, deleteCarWashbill, getCarWashBillByDate, updateCommissionStatus} = require("../../controller/carWash/carwashCtrl");
 const router = express.Router();
 // const UserAuthenication = require("../../middleware/UserAuthenication")
 const upload = require("../../middleware/upload");
@@ -9,6 +9,9 @@ const { saveOilShopProduct } = require("../../controller/oilShop/oilShopCtrl");
 
 router.post("/carWash-bill",  generatecarwashbillCtrl  );
 router.get("/carWash-bills",  getallbills);
+router.get("/commission-paid",  updateCommissionStatus);
+
+router.get("/carWash-bill-date",  getCarWashBillByDate);
 router.delete("/carWash-bill-delete/:id",  deleteCarWashbill);
 router.put("/carWash-bills-update/:id",  updateCarWashbill);
 router.post("/add-product" , upload.single("image") , saveOilShopProduct)
