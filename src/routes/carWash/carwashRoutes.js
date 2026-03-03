0fdfc47245ff2d2f11e6a1ccfa7d42f59f5da4d0
hamzasafdar43 <alihamza4443243@gmail.com>
2026-03-04 02:29:30 +0500

    write api for get monthly report

--- src/routes/carWash/carwashRoutes.js
+++ src/routes/carWash/carwashRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getallbills  ,  updateCarWashbill, deleteCarWashbill, getCarWashBillByDate, updateCommissionStatus} = require("../../controller/carWash/carwashCtrl");
+const { getallbills  ,  updateCarWashbill, deleteCarWashbill, getCarWashBillByDate, updateCommissionStatus, getCarWashDataMonthly} = require("../../controller/carWash/carwashCtrl");
 const router = express.Router();
 const UserAuthenication = require("../../middleware/UserAuthenication");
 
@@ -11,6 +11,7 @@ router.post("/commission-paid",UserAuthenication ,  updateCommissionStatus);
 router.get("/carWash-bill-date", UserAuthenication,  getCarWashBillByDate);
 router.delete("/carWash-bill-delete/:id", UserAuthenication,  deleteCarWashbill);
 router.put("/carWash-bills-update/:id", UserAuthenication,  updateCarWashbill);
+router.get("/carWash/monthly-data", UserAuthenication, getCarWashDataMonthly);
 
 
 
